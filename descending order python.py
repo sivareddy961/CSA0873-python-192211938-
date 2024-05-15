7fa3e124359b45fee4b86cae8dbfc1756ed26e82
sivareddy961 <133642401+sivareddy961@users.noreply.github.com>
2024-05-15 22:44:10 +0530

    Add files via upload

---
+++ descending order python.py
@@ -0,0 +1,4 @@
+# List of numbers
+numbers = [5, 2, 9, 1, 6]
+numbers.sort(reverse=True)
+print("Sorted numbers in descending order:", numbers)
---
+++ factorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers"
+    elif n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+number = int(input("Enter a number: "))
+print("Factorial of", number, "is:", factorial(number))
